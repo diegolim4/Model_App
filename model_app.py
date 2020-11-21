19b4f156c3c5172c298c4b57a879cbde9cd98505
Diego Lima <lima.j.diego@gmail.com>
2020-11-21 17:19:08 -0300

    Commite do modelo de aplicativo

---
+++ model_app.py
@@ -0,0 +1,42 @@
+from kivy.app import App
+from kivy.uix.screenmanager import ScreenManager, Screen
+
+
+class Gerenciador(ScreenManager):
+    pass
+
+
+class TelaInicial(Screen):
+    pass
+
+
+class Tela1(Screen):
+    pass
+
+
+class Tela2(Screen):
+    pass
+
+
+class Tela3(Screen):
+    pass
+
+
+class Tela4(Screen):
+    pass
+
+
+class Tela5(Screen):
+    pass
+
+
+class Tela6(Screen):
+    pass
+
+
+class Model_App(App):
+    def build(self):
+        return
+
+
+Model_App().run()
